73484382e3c94dd06b76d1f8efe469967a287c70
unknown <kimimarux@hotmail.com>
2022-03-27 11:03:07 -0300

    readywork

--- src/main/java/com/lipe/request/testeRequests.java
+++ src/main/java/com/lipe/request/testeRequests.java
@@ -8,14 +8,15 @@ import com.lipe.requests.utils.Utils;
 public class testeRequests {
 
 	public static void main(String[] args) {
-		
+		String authRequestHeader = "";
+		String authRequestBody = "";
 		String StringContent = "";
 		Entities e =  null;
 		EntitiesDefault ed = null;
 		
 		
 		HttpRequests request = new HttpRequests("aWS", 1010, "XABLAU", "XABLAU");
-		StringContent = request.shouldGetRequestsResponseBody("", 0, "chave<ALTERAR>valor", "chave<ALTERAR>valor");
+		StringContent = request.shouldGetRequestsResponseBody("", 0, authRequestHeader, authRequestBody);
 		//-------------------------JACKSON tranformacao--------
 		ed = Utils.transforma_json(StringContent, e);
         //-----------------------------------------------------
